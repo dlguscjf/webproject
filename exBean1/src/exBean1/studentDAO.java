0fa4e4a0e62766c45e23e7f1d9acabbfe3804b9b
dlgus <dlgus@DESKTOP-RS88I3Q>
2018-09-20 11:26:11 +0900

    add javabean

---
+++ exBean1/src/exBean1/studentDAO.java
@@ -0,0 +1,26 @@
+
+package exBean1;
+import java.sql.*;
+public class studentDAO {
+ public void insertStudent(studentVO item) throws Exception {
+  String no = item.getNo();
+  String name = item.getName();
+
+
+  Class.forName("oracle.jdbc.driver.OracleDriver");
+  Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","201521267","201521267");
+
+ 
+
+  PreparedStatement pstmt = conn.prepareStatement("insert into student values(?,?)");
+  pstmt.setString(1,no);
+  pstmt.setString(2,name);
+  pstmt.executeUpdate();
+
+ 
+
+  pstmt.close();
+
+  conn.close();
+ }
+}
---
+++ exBean1/src/exBean1/studentVO.java
@@ -0,0 +1,21 @@
+package exBean1;
+
+public class studentVO {
+	private String no;
+	private String name;
+	
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
