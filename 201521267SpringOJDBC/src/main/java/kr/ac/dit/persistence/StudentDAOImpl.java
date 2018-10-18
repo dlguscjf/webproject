952b66db7ed1b3f7b1f0ae6d2370753ac7ed3a5f
dlgus <dlgus@DESKTOP-RS88I3Q>
2018-10-18 11:01:08 +0900

    주석 추가

--- 201521267SpringOJDBC/src/main/java/kr/ac/dit/persistence/StudentDAOImpl.java
+++ 201521267SpringOJDBC/src/main/java/kr/ac/dit/persistence/StudentDAOImpl.java
@@ -13,18 +13,20 @@ public class StudentDAOImpl implements StudentDAO {
 	@Override
 	public void insertStudent(StudentVO studentVO) throws Exception {
 		  jdbcTemplate.update("insert into student values(?,?)", studentVO.getNo(), studentVO.getName());
+		  //기존의 방법 대신 한줄로 처리
 	}
 	@Override
 	public List<StudentVO> selectStudentList() throws Exception {
 		List<StudentVO> items = new ArrayList<StudentVO>();
 			    List<Map<String, Object>> rows = jdbcTemplate.queryForList("select * from student"); 
-			    for (@SuppressWarnings("rawtypes") Map row : rows) { 
+			    //map을 이용하여 간편하게 select문 사용
+			    for (@SuppressWarnings("rawtypes") Map row : rows) { //@SuppressWarnings("rawtypes")는 Map의 Warnings을 무시
 			      StudentVO item = new StudentVO(); 
 			      item.setNo((String) row.get("no")); 
 			      item.setName((String) row.get("name")); 
 			      items.add(item); 
 			    } 
 			    return items;
-	
 	}
 }
+
